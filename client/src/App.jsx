28c824aa73c54ea2a6507860223a987547fc5c45
Kozachenko Volodymyr <kz4nkovova@gmail.com>
2025-03-11 09:58:32 +0100

    Fixed Mistakes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,18 @@
 import "./App.scss";
 import { Navigate, Routes, Route } from "react-router";
-import { Provider } from "react-redux";
+import Header from "./Components/Header/Header.jsx";
 import Main from "./Components/Main/Main";
 import About from "./Components/About/About";
 import Hotels from "./Components/Hotels/Hotels";
-import { store } from "./store";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route
-          path="/main-page"
-          element={
-            <Provider store={store}>
-              <Main />
-            </Provider>
-          }
-        />
+        <Route path="/main-page" element={<Main />} />
         <Route path="*" element={<Navigate to="/main-page" replace />} />
         <Route path="/about" element={<About />} />
-        <Route
-          path="/hotels"
-          element={
-            <Provider store={store}>
-              {" "}
-              <Hotels />
-            </Provider>
-          }
-        />
+        <Route path="/hotels" element={<Hotels />} />
       </Routes>
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import Header from "./Components/Header/Header.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Header />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
